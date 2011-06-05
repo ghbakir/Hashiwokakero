9c39c826dbda89a5d98b5c51c073f38338ad4c37
Gökhan BakIr <g.h.bakir@gmail.com>
2011-06-05 12:02:47 +0200

    Added comments and helper functions to remove duplicate code.

--- src/de/fastray/BoardElement.java
+++ src/de/fastray/BoardElement.java
@@ -3,11 +3,57 @@ package de.fastray;
 import java.util.Vector;
 import android.util.Log;
 
+// This class holds information about a grid point.
+// Every grid point does have
+//   -- a coordinate = (row , col).
+//   -- a state "is_island" whether it is an island or empty.
+// If the grid point is an island then it furthermore contains
+//   -- number of possible other islands it connects to.
+//   -- possible connections to its neighbours N,S,E and W. 
 public class BoardElement {
+  // *********************************************************
+  // *********************************************************
+  // General members
+  // *********************************************************
+  // *********************************************************
+
   public int row = 0;
   public int col = 0;
-  public int max_connecting_bridges = 0;
+
+  // Is this an island?
   public boolean is_island = false;
+
+  // *********************************************************
+  // *********************************************************
+  // Island specific members.
+  // *********************************************************
+  // *********************************************************
+  public int max_connecting_bridges = 0;
+
+  // It is easier to refer to neighbours via directions.
+  public enum Direction {
+    EAST, SOUTH, WEST, NORTH;
+  }
+  
+  class Connection {
+    public BoardElement source;
+    public BoardElement destination;
+    public int second;
+    public Connection() {
+      source = null;
+      second = 0;
+    }
+    public Connection(BoardElement elt, int val) {
+      source = elt;
+      second = val;
+    }
+    public Connection clone() {
+      Connection c = new Connection();
+      c.source = source;
+      c.second = second;
+      return c;
+    }
+  }; // Connection
 
   // Pairs of a BoardElement and the number of connecting bridges
   public Connection connecting_north = null;
@@ -46,46 +92,26 @@ public class BoardElement {
     return elt;
   }
 
-  public int GetCurrentCount() {
-    int s = 0;
-    if (connecting_east != null) {
-      s += connecting_east.second;
+
+  private int GetConnectionCount(Connection connection){
+    if (connection == null) {
+      return 0;
+    } else {
+      return connection.second;
     }
-    if (connecting_south!= null) {
-      s += connecting_south.second;
-    }
-    if (connecting_north != null) {
-      s += connecting_north .second;
-    }
-    if (connecting_west != null) {
-      s += connecting_west.second;
-    }
-    return s;
   }
 
-  static class Connection {
-    public BoardElement source;
-    public BoardElement destination;
-    public int second;
-    public Connection() {
-      source = null;
-      second = 0;
+  // Return the current count of connections connected
+  // to this island.
+  public int GetCurrentCount() {
+    if (!is_island) {
+      return 0;
     }
-    public Connection(BoardElement elt, int val) {
-      source = elt;
-      second = val;
-    }
-
-    public Connection clone() {
-      Connection c = new Connection();
-      c.source = source;
-      c.second = second;
-      return c;
-    }
-  };
-
-  public enum Direction {
-    EAST, SOUTH, WEST, NORTH;
+    int s = GetConnectionCount(connecting_east);
+    s += GetConnectionCount(connecting_south);
+    s += GetConnectionCount(connecting_north);
+    s += GetConnectionCount(connecting_west);
+    return s;
   }
 
   void AddConnection(Direction dir, BoardElement dest, int value) {
@@ -109,4 +135,4 @@ public class BoardElement {
         break;
     }
   }
-};
+}; // BoardElement
